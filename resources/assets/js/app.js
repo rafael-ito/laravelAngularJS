566ecdf326ba444afad0786843f960b9841a2fd3
Rafael Ito <rafa_oak@hotmail.com>
2015-11-23 14:58:03 +0000

    Final Pasta 3

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,8 @@
-var app = angular.module('app', ['ngRoute', 'app.controllers']);
+var app = angular.module('app', ['ngRoute', 'angular-oauth2','app.controllers']);
 
-angular.module('app.controllers', []);
+angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
 
-app.config(function ($routeProvider) {
+app.config(['$routeProvider', 'OAuthProvider', function ($routeProvider, OAuthProvider) {
 	$routeProvider
 		.when('/login', {
 			templateUrl: 'build/views/login.html',
@@ -11,5 +11,28 @@ app.config(function ($routeProvider) {
 		.when('/home', {
 			templateUrl: 'build/views/home.html',
 			controller: 'HomeController'
-		})
-});
+		});
+	OAuthProvider.configure({
+		baseUrl: 'http://laravelangularjs.dev',
+		clientId: 'appid1',
+		clientSecret: 'secret', // opcional
+		grantPath: 'oauth/access_token'
+	});
+}]);
+
+app.run(['$rootScope', '$window', 'OAuth', function($rootScope, $window, OAuth) {
+    $rootScope.$on('oauth:error', function(event, rejection) {
+		// Ignore 'invalid_grant' error - should be catched on 'LoginController'.
+		if ('invalid_grant' === rejection.data.error) {
+			return;
+		}
+
+	    // Refresh token when a 'invalid_token' error occurs.
+	    if ('invalid_token' === rejection.data.error) {
+	        return OAuth.getRefreshToken();
+	    }
+
+	    // Redirect to '/login' with the 'error_reason'.
+	    return $window.location.href = '/login?error_reason=' + rejection.data.error;
+    });
+}]);
